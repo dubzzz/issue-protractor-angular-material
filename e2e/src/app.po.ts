ce5dbe4528ae28f23d07e371a74dff7e49c5134a
Nicolas DUBIEN <github@dubien.org>
2019-08-29 10:45:07 +0200

    Lint the code

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from "protractor";
+import { browser, by, element } from 'protractor';
 
 export class AppPage {
   async navigateTo() {
@@ -9,7 +9,7 @@ export class AppPage {
     await element(by.id(selectorName)).click();
     // await browser.sleep(100); // potential workaround or browser.wait
     await element
-      .all(by.cssContainingText("span.mat-option-text", value))
+      .all(by.cssContainingText('span.mat-option-text', value))
       .get(0)
       .click();
     // await browser.sleep(100); // here too? potential workaround or browser.wait
@@ -17,7 +17,7 @@ export class AppPage {
 
   async checkStatus(on: boolean) {
     const enabled =
-      (await element(by.id("send-it")).getAttribute("disabled")) === null;
+      (await element(by.id('send-it')).getAttribute('disabled')) === null;
     expect(enabled).toBe(on);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { MatButtonModule, MatSelectModule } from "@angular/material";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule, MatSelectModule } from '@angular/material';
 import {
   BrowserAnimationsModule,
   NoopAnimationsModule
-} from "@angular/platform-browser/animations";
+} from '@angular/platform-browser/animations';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
